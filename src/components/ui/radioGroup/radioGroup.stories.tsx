f1e02d4cb1f46d9e54db932b3e11de22c706f7c6
krawtschenko <eugenykravchenko@gmail.com>
2024-08-18 11:56:20 +0200

    fix: style RadioGroup

---
+++ src/components/ui/radioGroup/radioGroup.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta } from '@storybook/react'
+
+import { RadioGroup, RadioGroupItem } from './radioGroup'
+
+const meta = {
+  component: RadioGroup,
+  tags: ['autodocs'],
+  title: 'Components/UI/Radio Group',
+} satisfies Meta<typeof RadioGroup>
+
+export default meta
+
+export const Primary = {
+  render: () => {
+    return (
+      <RadioGroup>
+        <RadioGroupItem label={'Thor'} value={'Thor'} />
+        <RadioGroupItem label={'Hulk'} value={'Hulk'} />
+        <RadioGroupItem label={'Tony'} value={'Tony'} />
+        <RadioGroupItem label={'Thrall'} value={'Thrall'} />
+        <RadioGroupItem label={'Sydney'} value={'Sydney'} />
+      </RadioGroup>
+    )
+  },
+}
+
+export const Disabled = {
+  render: () => {
+    return (
+      <RadioGroup disabled>
+        <RadioGroupItem label={'Thor'} value={'Thor'} />
+        <RadioGroupItem label={'Hulk'} value={'Hulk'} />
+        <RadioGroupItem label={'Tony'} value={'Tony'} />
+        <RadioGroupItem label={'Thrall'} value={'Thrall'} />
+        <RadioGroupItem label={'Sydney'} value={'Sydney'} />
+      </RadioGroup>
+    )
+  },
+}
--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -7,14 +7,8 @@ import style from './radioGroup.module.scss'
 
 import { Typography } from '../typography/typography'
 
-type RadioGroupProps = { options: Options[] } & ComponentPropsWithoutRef<
-  typeof RadioGroupRadix.Root
->
-
-type Options = { label: string; value: string }
-
-export const RadioGroup = (props: RadioGroupProps) => {
-  const { className, disabled, options, ...rest } = props
+export const RadioGroup = (props: ComponentPropsWithoutRef<typeof RadioGroupRadix.Root>) => {
+  const { children, className, disabled, ...rest } = props
 
   return (
     <RadioGroupRadix.Root
@@ -23,19 +17,28 @@ export const RadioGroup = (props: RadioGroupProps) => {
       disabled={disabled}
       {...rest}
     >
-      {options.map(({ label, value }) => (
-        <div key={value} style={{ alignItems: 'center', display: 'flex' }}>
-          <div className={style.itemWrap}>
-            <RadioGroupRadix.Item className={style.radioGroupItem} id={value} value={value}>
-              <RadioGroupRadix.Indicator className={style.radioGroupIndicator} />
-            </RadioGroupRadix.Item>
-          </div>
-
-          <Typography as={'label'} className={style.label} htmlFor={value} variant={'body2'}>
-            {label}
-          </Typography>
-        </div>
-      ))}
+      {children}
     </RadioGroupRadix.Root>
+  )
+}
+
+type RadioGroupItemProps = {
+  label: string
+  value: string
+}
+
+export const RadioGroupItem = ({ label, value }: RadioGroupItemProps) => {
+  return (
+    <div key={value} style={{ alignItems: 'center', display: 'flex' }}>
+      <div className={style.itemWrap}>
+        <RadioGroupRadix.Item className={style.radioGroupItem} id={value} value={value}>
+          <RadioGroupRadix.Indicator className={style.radioGroupIndicator} />
+        </RadioGroupRadix.Item>
+      </div>
+
+      <Typography as={'label'} className={style.label} htmlFor={value} variant={'body2'}>
+        {label}
+      </Typography>
+    </div>
   )
 }
