ee6911b4a7d9959bd9be67a83e0d581152fb8dfa
krawtschenko <eugenykravchenko@gmail.com>
2024-09-06 00:39:46 +0200

    feat: added baseQueryWithReauth.ts

---
+++ src/features/api/baseQueryWithReauth.ts
@@ -0,0 +1,76 @@
+// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#preventing-multiple-unauthorized-errors
+
+import { BaseQueryFn, FetchArgs, FetchBaseQueryError, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { Mutex } from 'async-mutex'
+
+import { path } from '../../routes/path'
+import { router } from '../../routes/router'
+
+// create a new mutex
+const mutex = new Mutex()
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://api.flashcards.andrii.es',
+  prepareHeaders: headers => {
+    const accessToken = localStorage.getItem('accessToken')
+
+    if (headers.get('Authorization')) {
+      return headers
+    }
+
+    if (accessToken) {
+      headers.set('Authorization', `Bearer ${accessToken}`)
+    }
+  },
+})
+
+export const baseQueryWithReauth: BaseQueryFn<
+  FetchArgs | string,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // wait until the mutex is available without locking it
+  await mutex.waitForUnlock()
+
+  let result = await baseQuery(args, api, extraOptions)
+
+  if (result.error && result.error.status === 401) {
+    // checking whether the mutex is locked
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
+
+      try {
+        const refreshToken = localStorage.getItem('refreshToken')
+        const refreshResult: any = await baseQuery(
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+            method: 'POST',
+            url: '/v2/auth/refresh-token',
+          },
+          api,
+          extraOptions
+        )
+
+        if (refreshResult.data) {
+          localStorage.setItem('accessToken', refreshResult.data.accessToken)
+          localStorage.setItem('refreshToken', refreshResult.data.refreshToken)
+
+          // retry the initial query
+          result = await baseQuery(args, api, extraOptions)
+        } else {
+          await router.navigate(path.login)
+        }
+      } finally {
+        // release must be called once the mutex should be released again.
+        release()
+      }
+    } else {
+      // wait until the mutex is available without locking it
+      await mutex.waitForUnlock()
+      result = await baseQuery(args, api, extraOptions)
+    }
+  }
+
+  return result
+}
