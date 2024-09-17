600cdd2cad7fef3c2d427e9e4a3fc1e0c1a889cf
krawtschenko <eugenykravchenko@gmail.com>
2024-09-17 15:13:48 +0200

    fix: avatar

--- src/components/ui/avatar/avatar.tsx
+++ src/components/ui/avatar/avatar.tsx
@@ -12,31 +12,27 @@ type AvatarProps = {
 } & ComponentPropsWithoutRef<typeof AvatarRadix.Root>
 
 export const Avatar = ({ avatar, className, fallbackSize, name, ...rest }: AvatarProps) => {
+  // Create a fallback using the first letters of the name
   const fallback = name
     ?.split(' ')
     .slice(0, 2)
     .map(word => word[0])
     .join('')
 
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF'
-    let color = '#'
+  // Generate two colors for the gradient background
+  const color1 = useMemo(() => getRandomColor(), [])
+  const color2 = useMemo(() => getRandomColor(), [])
 
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * letters.length)]
-    }
+  // Determine text color (white or black) based on the presence of '0' or '1' in the colors
+  const textColor = useMemo(
+    () => (shouldUseWhiteText(color1) || shouldUseWhiteText(color2) ? '#FFFFFF' : '#000000'),
+    [color1, color2]
+  )
 
-    return color
-  }
-
-  const shouldUseWhiteText = (color: string) => {
-    return color.includes('0') || color.includes('1') || color.includes('2')
-  }
-
-  const backgroundColor = useMemo(() => getRandomColor(), [])
-  const color = useMemo(
-    () => (shouldUseWhiteText(backgroundColor) ? '#f4f2fa' : '#000'),
-    [backgroundColor]
+  // Create the CSS gradient string using the two colors
+  const gradientBackground = useMemo(
+    () => `linear-gradient(135deg, ${color1}, ${color2})`,
+    [color1, color2]
   )
 
   return (
@@ -46,10 +42,27 @@ export const Avatar = ({ avatar, className, fallbackSize, name, ...rest }: Avata
       <AvatarRadix.Fallback
         className={style.avatarFallback}
         delayMs={0}
-        style={{ backgroundColor, color, fontSize: fallbackSize }}
+        style={{ background: gradientBackground, color: textColor, fontSize: fallbackSize }} // Применяем градиент и цвет текста
       >
         {fallback}
       </AvatarRadix.Fallback>
     </AvatarRadix.Root>
   )
+}
+
+// Function to generate a random color in hex format
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+
+  return color
+}
+
+// Function to check if the color contains '0' or '1'
+const shouldUseWhiteText = (color: string) => {
+  return color.includes('0') || color.includes('1') || color.includes('2')
 }
