debca0573678268f569fa02655b17ee9be97ee1a
krawtschenko <eugenykravchenko@gmail.com>
2024-10-01 13:01:41 +0200

    fix: style

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -79,7 +79,6 @@ export const Pagination = (props: PaginationProps) => {
       >
         <SelectItem value={'5'}>5</SelectItem>
         <SelectItem value={'10'}>10</SelectItem>
-        <SelectItem value={'30'}>30</SelectItem>
         <SelectItem value={'50'}>50</SelectItem>
         <SelectItem value={'100'}>100</SelectItem>
       </Select>
