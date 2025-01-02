73eafe57f212772f74f1d9fa317a72198f166893
krawtschenko <eugenykravchenko@gmail.com>
2025-01-02 12:58:51 +0100

    Updates

---
+++ .storybook/preview.ts
@@ -0,0 +1,32 @@
+import type { Preview } from '@storybook/react'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import '../src/styles/index.scss'
+import { themes } from '@storybook/theming'
+import { withRouter } from 'storybook-addon-remix-react-router'
+
+const preview: Preview = {
+  decorators: [withRouter],
+  parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'dark',
+          value: '#000000',
+        },
+      ],
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+}
+
+export default preview
