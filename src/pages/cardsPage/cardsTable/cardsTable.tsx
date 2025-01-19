e5f86062ded92e7ac7497824f44e6b263dffe998
krawtschenko <eugenykravchenko@gmail.com>
2025-01-19 14:17:07 +0100

    fix: card table

--- src/pages/cardsPage/cardsTable/cardsTable.tsx
+++ src/pages/cardsPage/cardsTable/cardsTable.tsx
@@ -43,9 +43,7 @@ export const CardsTable = (props: CardsTableProps) => {
             onClick={() => handleSort('question', orderBy, setOrderBy)}
           >
             <div>
-              Question Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam commodi
-              consequuntur culpa doloremque fugit id minima molestiae praesentium sit. Ab ad, cumque
-              eaque error nam numquam possimus reprehenderit suscipit ullam?
+              Question
               {getSortIcon('question', orderBy)}
             </div>
           </Th>
@@ -79,126 +77,78 @@ export const CardsTable = (props: CardsTableProps) => {
             <div>Grade {getSortIcon('grade', orderBy)}</div>
           </Th>
 
-          {isOwner && <Th className={style.th}></Th>}
+          {isOwner && <Th className={clsx(style.th, style.thActions)}></Th>}
         </Tr>
       </Thead>
-    </Table>
 
-    // <Table className={clsx(style.table, className, isOwner && style.withActions)}>
-    //   <Thead>
-    //     <Tr>
-    //       <Th
-    //         className={clsx(
-    //           style.th,
-    //           style.thQuestion,
-    //           getSortIcon('question', orderBy) && style.active
-    //         )}
-    //         onClick={() => handleSort('question', orderBy, setOrderBy)}
-    //       >
-    //         <div>Question {getSortIcon('question', orderBy)}</div>
-    //       </Th>
-    //
-    //       <Th
-    //         className={clsx(
-    //           style.th,
-    //           style.thAnswer,
-    //           getSortIcon('answer', orderBy) && style.active
-    //         )}
-    //         onClick={() => handleSort('answer', orderBy, setOrderBy)}
-    //       >
-    //         <div>Answer {getSortIcon('answer', orderBy)}</div>
-    //       </Th>
-    //
-    //       <Th
-    //         className={clsx(
-    //           style.th,
-    //           style.thUpdated,
-    //           getSortIcon('updated', orderBy) && style.active
-    //         )}
-    //         onClick={() => handleSort('updated', orderBy, setOrderBy)}
-    //       >
-    //         <div>Last Updated {getSortIcon('updated', orderBy)}</div>
-    //       </Th>
-    //
-    //       <Th
-    //         className={clsx(style.th, style.thGrade, getSortIcon('grade', orderBy) && style.active)}
-    //         onClick={() => handleSort('grade', orderBy, setOrderBy)}
-    //       >
-    //         <div>Grade {getSortIcon('grade', orderBy)}</div>
-    //       </Th>
-    //
-    //       {isOwner && <Th className={style.th}></Th>}
-    //     </Tr>
-    //   </Thead>
-    //
-    //   <Tbody>
-    //     {cards?.map(({ answer, answerImg, grade, id, question, questionImg, updated }) => {
-    //       const updatedLocale = new Date(updated).toLocaleDateString('en-GB')
-    //
-    //       const questionCover = questionImg || coverImg
-    //       const answerCover = answerImg || coverImg
-    //
-    //       // Generate stars
-    //       const stars = Array.from({ length: 5 }, (_, index) =>
-    //         index < grade ? <IoIosStar key={index} /> : <IoIosStarOutline key={index} />
-    //       )
-    //
-    //       return (
-    //         <Tr key={id}>
-    //           <CardsDialog
-    //             answer={answer}
-    //             answerImg={answerImg}
-    //             onOpenChange={() => setOpenModalId(null)}
-    //             onSubmit={body => onUpdateCard({ id, ...body })}
-    //             open={openModalId === id}
-    //             question={question}
-    //             questionImg={questionImg}
-    //             title={'Edit Card'}
-    //           />
-    //
-    //           <CardDialogDelete
-    //             onOpenChange={() => setOpenDeleteModalId(null)}
-    //             onSubmit={() => onDeleteCard(id)}
-    //             open={openDeleteModalId === id}
-    //           />
-    //
-    //           <Td className={clsx(style.td, style.tdQuestion)} title={question}>
-    //             <div className={style.content}>
-    //               <img alt={'cover'} className={style.cover} src={questionCover} />
-    //               <span className={style.text}>{question}</span>
-    //             </div>
-    //           </Td>
-    //
-    //           <Td className={clsx(style.td, style.tdAnswer)} title={answer}>
-    //             <div className={style.content}>
-    //               <img alt={'cover'} className={style.cover} src={answerCover} />
-    //               <span className={style.text}>{answer}</span>
-    //             </div>
-    //           </Td>
-    //
-    //           <Td className={clsx(style.td, style.tdUpdated)}>{updatedLocale}</Td>
-    //
-    //           <Td className={clsx(style.td, style.tdGrade)}>
-    //             <div className={style.stars}>{stars}</div>
-    //           </Td>
-    //
-    //           {isOwner && (
-    //             <Td className={clsx(style.td, style.tdActions)}>
-    //               <div>
-    //                 <Button className={style.edit} onClick={() => setOpenModalId(id)}>
-    //                   <FiEdit />
-    //                 </Button>
-    //
-    //                 <Button className={style.btnTrash} onClick={() => setOpenDeleteModalId(id)}>
-    //                   <FiTrash />
-    //                 </Button>
-    //               </div>
-    //             </Td>
-    //           )}
-    //         </Tr>
-    //       )
-    //     })}
-    //   </Tbody>
-    // </Table>
+      <Tbody>
+        {cards?.map(({ answer, answerImg, grade, id, question, questionImg, updated }) => {
+          const updatedLocale = new Date(updated).toLocaleDateString('en-GB')
+
+          const questionCover = questionImg || coverImg
+          const answerCover = answerImg || coverImg
+
+          // Generate stars
+          const stars = Array.from({ length: 5 }, (_, index) =>
+            index < grade ? <IoIosStar key={index} /> : <IoIosStarOutline key={index} />
+          )
+
+          return (
+            <Tr key={id}>
+              <CardsDialog
+                answer={answer}
+                answerImg={answerImg}
+                onOpenChange={() => setOpenModalId(null)}
+                onSubmit={body => onUpdateCard({ id, ...body })}
+                open={openModalId === id}
+                question={question}
+                questionImg={questionImg}
+                title={'Edit Card'}
+              />
+
+              <CardDialogDelete
+                onOpenChange={() => setOpenDeleteModalId(null)}
+                onSubmit={() => onDeleteCard(id)}
+                open={openDeleteModalId === id}
+              />
+
+              <Td className={clsx(style.td, style.tdQuestion)} title={question}>
+                <div className={style.content}>
+                  <img alt={'cover'} className={style.cover} src={questionCover} />
+                  <span className={style.text}>{question}</span>
+                </div>
+              </Td>
+
+              <Td className={clsx(style.td, style.tdAnswer)} title={answer}>
+                <div className={style.content}>
+                  <img alt={'cover'} className={style.cover} src={answerCover} />
+                  <span className={style.text}>{answer}</span>
+                </div>
+              </Td>
+
+              <Td className={clsx(style.td, style.tdUpdated)}>{updatedLocale}</Td>
+
+              <Td className={clsx(style.td, style.tdGrade)}>
+                <div className={style.stars}>{stars}</div>
+              </Td>
+
+              {isOwner && (
+                <Td className={clsx(style.td, style.tdActions)}>
+                  <div>
+                    <Button className={style.edit} onClick={() => setOpenModalId(id)}>
+                      <FiEdit />
+                    </Button>
+
+                    <Button className={style.btnTrash} onClick={() => setOpenDeleteModalId(id)}>
+                      <FiTrash />
+                    </Button>
+                  </div>
+                </Td>
+              )}
+            </Tr>
+          )
+        })}
+      </Tbody>
+    </Table>
   )
 }
